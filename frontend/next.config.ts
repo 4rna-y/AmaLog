47903011991b4cb9bc443f61d4a76f3e5ac7a045
ama <ikuyama_kyo@outlook.jp>
2025-11-16 14:03:39 +0000

    Impl: add backend to image config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   images: {
     remotePatterns: [
       {
         protocol: "http",
         hostname: "localhost",
+        port: "8000"
+      },
+      {
+        protocol: "http",
+        hostname: "backend",
         port: "8000"
       }
     ]
