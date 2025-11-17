df298caafaec065293d34ed8388ba52176ec1732
ama <ikuyama_kyo@outlook.jp>
2025-11-17 05:23:56 +0000

    Fix: change page configuration it render dynamically

--- frontend/src/app/sitemap.ts
+++ frontend/src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next'
 import { BlogApi } from '@/api/blog'
 
+export const dynamic = 'force-dynamic';
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = 'https://arnay.net'
 
