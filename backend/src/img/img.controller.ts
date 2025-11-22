b46ad72f8cfc35bd27175d74a92159ccbdac0cd8
ama <ikuyama_kyo@outlook.jp>
2025-11-22 08:44:43 +0000

    Fix: change static plugin to enable dynamic file discovery

--- backend/src/img/img.controller.ts
+++ backend/src/img/img.controller.ts
@@ -1,19 +1,24 @@
 import jwt from "@elysiajs/jwt";
-import Elysia from "elysia";
+import Elysia, { NotFoundError } from "elysia";
 import { imgModels } from "./img.model";
 import { ImgService } from "./img.service";
-import staticPlugin from "@elysiajs/static";
 
 const uploadsPath = process.env.NODE_ENV === "production" ? "/app/uploads" : "./uploads";
 
 export const imgController = new Elysia()
     .use(jwt({ name: "jwt", secret: process.env.JWT_SECRET!! }))
     .use(imgModels)
-    .use(staticPlugin({
-        assets: uploadsPath,
-        prefix: "/static",
-    }))
     .decorate({ "img": ImgService })
+    .get("/static/*", async ({ params }) => {
+        const filePath = (params as any)['*'];
+        const file = Bun.file(`${uploadsPath}/${filePath}`);
+
+        if (!(await file.exists())) {
+            throw new NotFoundError();
+        }
+
+        return file;
+    })
     .post("/img", async ({ img, jwt, cookie: { auth }, query, body }) =>
         await img.post(jwt, auth, query, body),
         { query: "get-img-dto", body: "post-img-dto"})
